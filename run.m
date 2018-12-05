99e5acd3743cd8d9c3829566782d2b0152d9f32e
Harsh bhakta <habhakta@ucsc.edu>
2018-12-04 19:21:52 -0800

    Added Trajectory Plotting and a new input command that dictates if the user wants a simulation or a trajectory plot

--- run.m
+++ run.m
@@ -3,19 +3,19 @@
 %										run.m
 %--------------------------------------------------------------------------
 %**************************************************************************
-% @ Author                   : 		Yegeta Zeleke                                            	        
+% @ Author                   : 		Yegeta Zeleke and Harsh Bhakta                                            	        
 % @file_name				 : 		 run.m														  
 % @ Date                     : 	   11/02/18                                                     
 % @ Discription				 :      Motion Planning Framework by which a
 %                                   user choose a planning algorithm and
 %                                   a vehicle dynamics and run simulations
 %
-% @ Usage					: run('model','planner','environment', 'options', sim_time)    																						  
+% @ Usage					: run('model','planner','environment', 'options', sim_time, flag)    																						  
 %@Revision					:  	11/5/18                                                                                      
 %***************************************************************************
 
 %%
-function run(Vmodel,Planner,Environment,options,SimTime)    
+function run(Vmodel,Planner,Environment,options,SimTime, flag)    
 
     %close all windows but do not clear workspace otherwise we loose all params.
     close all;
@@ -39,20 +39,25 @@ function run(Vmodel,Planner,Environment,options,SimTime)
 
    
    %% Set simulation time
-    
-    %% Chose vehicle model
-    switch lower(Planner)
-        case 'mpc'
-               MpcPlanner(SimTime);
-               plotResult();
-        case 'rrt'
-               RRTPlanner(SimTime);
-               plotResult();
-        case 'astar'
-               AstarPlanner();
+    switch flag
+        case 0
+            switch lower(Planner)
+                case 'mpc'
+                    MpcPlanner(SimTime);
+                    plotResult();
+                case 'rrt'
+                    RRTPlanner(SimTime);
+                    plotResult();
+                case 'astar'
+                    AstarPlanner();
+                otherwise
+                    disp('Vehicle model not found in database')
+            end
         otherwise
-            disp('Vehicle model not found in database')
+            plotTrajectory(SimTime)
     end
+    %% Chose vehicle model
+    
     
 
     %% plot results
---
+++ sim/plotTrajectory.m
@@ -0,0 +1,26 @@
+function plotTrajectory(SimTime)
+    load('environment.mat');
+    
+    
+    MpcPlanner(SimTime);
+    load('result.mat');
+    
+    
+    figure(1)
+    plot(obstacle.X,obstacle.Y, 'rx', 'LineWidth',2);
+    rectangle('Position',[obstacle.rrX,obstacle.rrY,obstacle.Length,obstacle.Width],'EdgeColor','r');
+    hold on
+    plot(goal(1),goal(2), 'rx', 'LineWidth',2);
+    hold on
+    
+    
+    plot(ydata(1, :), ydata(2, :), 'b', 'LineWidth',2);
+    hold on;
+    RRTPlanner(SimTime);
+    load('result.mat');
+    plot(ydata(1, :), ydata(2, :), 'r', 'LineWidth',2);
+    hold on;
+    legend('obstacle', 'target', 'MPC','RRT')
+    title('Trajectory Comparision')
+end
+
