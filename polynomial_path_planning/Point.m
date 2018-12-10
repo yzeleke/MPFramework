b6daff5576d082194c663d65dee89258bd2669b1
David Kooi <kooi.david.w@gmail.com>
2018-12-09 17:21:07 -0800

    Added exponential to heuristic function. Added obstance sets.

---
+++ polynomial_path_planning/Point.m
@@ -0,0 +1,13 @@
+classdef Point
+    properties
+       x
+       y
+    end
+    
+    methods
+        function self = Point(i_x, i_y)
+            self.x = i_x;
+            self.y = i_y;
+        end
+    end
+end
