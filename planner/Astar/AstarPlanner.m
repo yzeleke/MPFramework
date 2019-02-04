3d89018e7b7d15a062daf0b11e16223cf64e3f72
Adam Ames <adrames@ucsc.edu>
2019-02-04 13:59:34 -0800

    added Astar diagonal

--- planner/Astar/AstarPlanner.m
+++ planner/Astar/AstarPlanner.m
@@ -21,7 +21,7 @@ function AstarPlanner(simT)
 %     PRIORITY_MOVEMENT = 5;
     
     %step size
-    xStep=5;
+    xStep=10;
     yStep=1;
 
     xMax = 100;%upper_bound_x;
@@ -42,13 +42,13 @@ function AstarPlanner(simT)
  	push(pq, startCell, 0);
     %fprintf("start (%f, %f), obstacle (%f, %f)\n", startX, startY, obstacle.X, obstacle.Y);
     
-    previous = repmat(mapCell(10000, 0, 0), xMax, yMax, 360);
-
+    map = repmat(mapCell(10000, 0, 0), xMax, yMax);
     while(isEmpty(pq) == 0)
 		current=pop(pq);
         if(floor(current.x) == targetX)
             if(floor(current.y) == targetY)% & abs(current.theta-targetTheta)<=5)
                 fprintf("Reached target.\n");
+                fprintf("current %d %d\n", current.x, current.y);
                 %imshow(map);
                 %current.change=PRIORITY_OBSTACLE_NEAR*(map.obs_dist_max-map.nearest_obstacle_distance(current))/(map.obs_dist_max-1) + fabs(current.theta)/BOT_M_ALPHA +1; 
                 x1data(1) = current.x;
@@ -56,9 +56,14 @@ function AstarPlanner(simT)
 			
                 while(current.x~=startX || current.y~=startY)% || current.theta~=start.theta)
 				%current.velocity=VELOCITY_MAX/current.change;
-                    Dummy=previous(floor(current.x), floor(current.y), floor(current.theta)+180);
+                    DummyX=map(current.x, current.y).previousX;
+                    DummyY=map(current.x, current.y).previousY;
     				%Dummy.change=PRIORITY_MOVEMENT*fabs(Dummy.theta-current.theta)/(2.0*BOT_M_ALPHA)+PRIORITY_OBSTACLE_NEAR*(map.obs_dist_max-map.nearest_obstacle_distance(Dummy))/(map.obs_dist_max-1)+fabs(Dummy.theta)/BOT_M_ALPHA+1;
-        			current=Dummy;
+%                    disp(current);
+        			current.x=DummyX;
+                    current.y=DummyY;
+%                    disp(current);
+%                    fprintf("current %d %d\n", current.x, current.y);
                     x1data(end+1) = current.x;
                     y1data(end+1) = current.y;
                 end
@@ -69,32 +74,33 @@ function AstarPlanner(simT)
 
         %next=current.getNextStates();
         %begin getNextStates
-        next = repmat(mapCell(0, 0, 0), 3, 1);
+        next = repmat(mapCell(0, 0, 0), 5, 1);
 %         stateCount = 1;
 
 %         for alpha=-BOT_M_ALPHA : BOT_M_ALPHA : BOT_M_ALPHA+0.001
-        for stateCount = 1:1:3
+        for stateCount = 1:1:5
 %             beta=d*tan(alpha*pi/180)/BOT_L;
-            if(stateCount == 2)%abs(beta)<0.001)
+            if(stateCount == 3)%abs(beta)<0.001)
                 next(stateCount).x=current.x+xStep;
 %                 next(stateCount).x=current.x+d*cos(current.theta*2.0*pi/Theta);
                 %fprintf("updated %f", next(stateCount).x);
                 next(stateCount).y=current.y;
-                next(stateCount).theta = 2;
 %                 next(stateCount).y=current.y+d*sin(current.theta*2.0*pi/Theta);
 %                 next(stateCount).theta=current.theta;
-            else
+            elseif (stateCount == 2 || stateCount == 4)
 %                 r=BOT_L/tan(alpha*pi/180);
                 next(stateCount).x=current.x;
 %                 next(stateCount).x=current.x+r*sin(current.theta*2.0*pi/Theta+beta)-r*sin(current.theta*2.0*pi/Theta);
-                next(stateCount).y=current.y+(stateCount-2)*yStep;
-                next(stateCount).theta = stateCount;
+                next(stateCount).y=current.y+(stateCount-3)*yStep;
 %                 next(stateCount).y=current.y-r*cos(current.theta*2.0*pi/Theta+beta)+r*cos(current.theta*2.0*pi/Theta);
 %                 if(current.theta + beta*180/pi/Theta_Res>0)
 %         			next(stateCount).theta=mod(current.theta + beta*180/pi/Theta_Res,Theta);
 %                 else
 %                     next(stateCount).theta=current.theta + beta*180/pi/Theta_Res+Theta;
 %                 end
+            else
+                next(stateCount).x=current.x+xStep;
+                next(stateCount).y=current.y+((stateCount-3)/2)*yStep;
             end
 %             stateCount = stateCount + 1;
 %            n.gx=n.x/Grid_Res;
@@ -103,19 +109,21 @@ function AstarPlanner(simT)
 %            next.push_back(n);
         end
 
-        for i=1:1:3
-            if(next(i).x >= 1 && next(i).x <= xMax && next(i).y >= 1 && next(i).y <= yMax)
+        for i=1:1:5
+            if(next(i).x >= 1 && next(i).x <= xMax && next(i).y >= 1 && next(i).y <= yMax && map(next(i).x, next(i).y).closed == 0)
                 if ~(next(i).x >= obstacle.rlSafeX && next(i).x <= obstacle.flSafeX && next(i).y >= obstacle.frSafeY && next(i).y <= obstacle.flSafeY) %if(~map.checkCollision(next(i)))
-                    if(i==2)
-            			next(i).cost3d=current.cost3d+1;
-                    else
-    					next(i).cost3d=current.cost3d+(floor(next(i).y)-targetY);
-                    end
-                    current.next=next(i);
-                    next(i).previousX=current.x;
-                    next(i).previousY=current.y;
-                    push(pq, next(i), next(i).cost3d + (5*hypot(targetX - floor(next(i).x), floor(targetY - next(i).y))));%pq.push(next(i));
-                    previous(floor(next(i).x), floor(next(i).y), floor(next(i).theta)+180)=current;
+%                    if(i==2)
+            			next(i).cost3d=current.cost3d+hypot(next(i).x-targetX, next(i).y-targetY);
+%                    else
+%    					next(i).cost3d=current.cost3d+(floor(next(i).y)-targetY);
+%                    end
+                    map(next(i).x, next(i).y).closed=1;
+                    map(next(i).x, next(i).y).previousX=current.x;
+                    map(next(i).x, next(i).y).previousY=current.y;
+%                       disp(next(i));
+%                    current.next=next(i);
+                    push(pq, next(i), 2*next(i).cost3d);% + (5*hypot(targetX - next(i).x, targetY - next(i).y)));%pq.push(next(i));
+%                    map(next(i).x, next(i).y)=current;
                 end
             end
         end
@@ -123,5 +131,4 @@ function AstarPlanner(simT)
     astar_time = toc(astar_start);
     ydata = [x1data; y1data];
     save('results/resultAstar.mat', 'ydata', 'astar_time', 'astar_start', 'goal');
-    fprintf("pq empty\n");
 end
